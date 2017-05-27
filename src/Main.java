dd31daa4d8123d073164d28fc8ae49a3c6688581
Şeyma Yılmaz <seyma.yilmaz3@ogr.sakarya.edu.tr>
2017-05-27 18:18:50 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Şeyma Yılmaz on 27.5.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Mathematic mathematic = new Mathematic();
+        mathematic.abs(2);
+        mathematic.abs(2.3);
+        
+    }
+}
---
+++ src/Mathematic.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Şeyma Yılmaz on 27.5.2017.
+ */
+public class Mathematic {
+
+    public int abs(int number){
+        if(number < 0){
+            return -number;
+        }
+
+        return number;
+    }
+
+    public double abs(double number){
+        if(number < 0){
+            return -number;
+        }
+
+        return number;
+    }
+
+}
